952822975813416eaf294540ed5a14bce3ec0900
majkel1989 <michal.jatczyszyn@gmail.com>
2019-08-19 17:48:57 +0100

    Added parameter DTO

---
+++ DTO/Parameters.cs
@@ -0,0 +1,10 @@
+namespace BluePrismTechnicalTest.DTO
+{
+    public class Parameters
+    {
+        public string DictionaryFile { get; set; }
+        public string StartWord { get; set; }
+        public string EndWord { get; set; }
+        public string ResultFile { get; set; }
+    }
+}
--- UI/ConsoleInterface.cs
+++ UI/ConsoleInterface.cs
@@ -1,4 +1,5 @@
 using System;
+using BluePrismTechnicalTest.DTO;
 
 namespace BluePrismTechnicalTest.UI
 {
@@ -6,17 +7,23 @@ namespace BluePrismTechnicalTest.UI
     {
         public void Init()
         {
+            var parameters = new Parameters();
+
             CreateMessage(new string[] { "#########################################################",
                                          "############### Blue Prism Technical Test ###############",
                                          "#########################################################"});
 
             CreateMessage(new string[]{"", "Give Dictionary File Name", "Dictionary File Name: "});
+            parameters.DictionaryFile = Console.ReadLine();
 
             CreateMessage(new string[]{ "", "Give Start Word (Letter case does matter)", "Start Word: "});
+            parameters.StartWord = Console.ReadLine();
 
             CreateMessage(new string[]{ "", "Give End Word (Letter case does matter)", "End Word: "});
+            parameters.EndWord = Console.ReadLine();
 
             CreateMessage(new string[]{ "", "Give Result File Name", "Result File Name: "});
+            parameters.ResultFile = Console.ReadLine();
         }
 
         private void CreateMessage(string[] messages)
